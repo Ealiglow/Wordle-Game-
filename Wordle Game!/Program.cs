9f69640a55c4dc72959b27397b174f9d77bc0121
Ealiglow <alva.oskarsson@elev.ga.lbs.se>
2025-04-07 14:37:57 +0200

    updated it lol, fixed bugs and added med terms

--- Wordle Game!/Program.cs
+++ Wordle Game!/Program.cs
@@ -10,14 +10,18 @@ bool game= true; //the bool that holds the game
 char restart; //to take in if you wanna restart
 bool won = false; //to stop the guessing if you win and let you know
 int gamesWon = 0; //checks how many games you have won to display
+char[] alphabet = new char[] {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z', };//All letters the user can put in to help stopping bugs when a user puts in a non ascii symbol
 Random rnd = new();
+bool Medical = false;
 string[] words = new string[] { "HELLO","SKIBIDI", "SIGMA", "ALPHA", "INDEX" };//Words are all that computer can choose from
+string[] Hardwords = new string[] { "ANAEMIA", "ANTIBIOTICS", "CHOLESTEROL", "CONFIDENTIALITY", "DIATHERMY", "ENZYME", "FIBROIDS", "GENE", "HAEMOLYSIS", "INFECTIOUS", "KETONES", "LUPUS", "MESH", "NEONATALOLOGIST", "OEDEMA", "PAEDIATRICIAN", "RETROVIRUS", "SCREENING", "TACHYCARDIA", "ULTRASOUND", "VARICELLA", "WOMB" };//Medical Words that the computer can choose from
 int[] chances= new int[] {4,7,10};//Chances are how many rows exists, incase there will be different dificulties
 List<char> lettersGuessed= new List<char>(); //list of all letters you guess echa round
 int maxAttempts= chances[1]; //checks how many # it should write acording to chances
 while (game == true)
 {
     int windex = rnd.Next(words.Length); //randomises the word you get
+    int Mwindex = rnd.Next(Hardwords.Length); //randomises the word you get
     Console.WriteLine("--------------------------------------------------------");
     Console.Write("\x1b[38;2;255;105;180m");
     Console.WriteLine("Wordle");
@@ -32,6 +36,7 @@ while (game == true)
     Console.WriteLine("Games won;");
     Console.WriteLine($"{gamesWon}");
     Console.Write("\x1b[0m"); //Resets colour back
+    Console.WriteLine("");
     Console.WriteLine("__________________________");
     Console.WriteLine("");
     Console.WriteLine("Colour guide;");
@@ -50,6 +55,7 @@ while (game == true)
     Console.ResetColor();
     Console.Write("= The letter isnt a part of the word");
     Console.WriteLine("");
+    Console.WriteLine("(Medical is extra hard and includes medical terms instead of the normal words)");
     Console.WriteLine("Difficulties;");
     
     Console.ForegroundColor = ConsoleColor.Green;
@@ -60,14 +66,18 @@ while (game == true)
     Console.Write("Medium[M]");
     Console.ResetColor();
    
-    Console.ForegroundColor = ConsoleColor.DarkRed;
+    Console.ForegroundColor = ConsoleColor.Red;
     Console.Write("Hard[H]");
     Console.ResetColor();
-   
+
+    Console.ForegroundColor = ConsoleColor.DarkRed;
+    Console.Write("Medical[A]");
+    Console.ResetColor();
+
     Console.WriteLine("");
     userinput = char.ToLower(Console.ReadKey().KeyChar);
-    Console.WriteLine("");
-    //checks and switches which difficulty you wanna play
+    //checks and switches which difficulty you would prefer to play on
+   
     if (userinput == 'h')
     {
         maxAttempts = chances[0];
@@ -85,22 +95,38 @@ while (game == true)
         number = maxAttempts;
 
     }
+    else if (userinput == 'A')
+    {
+        maxAttempts = chances[1];
+        number = maxAttempts;
+        Medical = true;
+
+    }
     else
     {
         Console.WriteLine("Please enter a valid letter");
         userinput = char.ToLower(Console.ReadKey().KeyChar);
     }
-
+    Console.WriteLine("");
     Console.WriteLine($"you have {number} chances right now, if you guess the right word then you win and if you do not then you loose");
     Console.WriteLine("");
     string[] guesses = new string[maxAttempts];
     //Adds the right amount of # into an array and then transforms it into a string
-    for (int i = 0; i < guesses.Length; i++)
+    if (Medical == true)
     {
-        guesses[i] = new string('#', words[windex].Length);
+        for (int i = 0; i < guesses.Length; i++)
+        {
+            guesses[i] = new string('#', words[Mwindex].Length);
 
+        }
     }
-  //positions the coursor right and writes up everything from the string above 
+    for (int i = 0; i < guesses.Length; i++)
+        {
+            guesses[i] = new string('#', words[windex].Length);
+
+        }
+    //fixes the words it uses to be the ones in the right difficulty
+    //positions the coursor right and writes up everything from the string above 
     for (int i = 0; i < guesses.Length; i++)
     {
 
@@ -111,99 +137,184 @@ while (game == true)
     Console.WriteLine("");
  
     Console.WriteLine("--------------------------------------------------------");
-    
+
     //all mechanics in the game
     for (int attempt = 0; attempt < maxAttempts; attempt++)
     {
 
         string wordGuessed = "";
-
-        do
+        //checks if it should use the medical words and lists or nor
+        if (Medical == true)
         {
-
+            do
             {
+
                 //changes coursor position everytime you guess
-                Console.SetCursorPosition(left + wordGuessed.Length, top + attempt); 
+                Console.SetCursorPosition(left + wordGuessed.Length, top + attempt);
                 //adds all the letters as chars into a list
+
                 char newChar = char.ToUpper(Console.ReadKey().KeyChar);
-                if (!newChar.Equals(lettersGuessed))
+                //chcks so its a new letter and that it is an letter before adding it so if it is not a letter you have to rewrite the letter
+                if (!newChar.Equals(lettersGuessed) && alphabet.Contains(newChar))
                 {
                     lettersGuessed.Add(newChar);
                 }
+
                 //adds all leters from wordsGuessed int lettersGuessed
                 wordGuessed = string.Concat(lettersGuessed);
+
             }
 
-        }
-        //limits how much you can write on one line
-        while (wordGuessed.Length != words[windex].Length);
 
+
+            while (wordGuessed.Length != words[Mwindex].Length);
+
+            //limits how much you can write on one line
+
+            while (wordGuessed.Length != words[Mwindex].Length) ;
+        }
+        else
+        {
+            do
+            {
+
+                //changes coursor position everytime you guess
+                Console.SetCursorPosition(left + wordGuessed.Length, top + attempt);
+                //adds all the letters as chars into a list
+
+                char newChar = char.ToUpper(Console.ReadKey().KeyChar);
+                //chcks so its a new letter and that it is an letter before adding it so if it is not a letter you have to rewrite the letter
+                if (!newChar.Equals(lettersGuessed) && alphabet.Contains(newChar))
+                {
+                    lettersGuessed.Add(newChar);
+                }
+
+                //adds all leters from wordsGuessed int lettersGuessed
+                wordGuessed = string.Concat(lettersGuessed);
+
+            }
+
+
+
+            while (wordGuessed.Length != words[Mwindex].Length);
+
+            //limits how much you can write on one line
+
+            while (wordGuessed.Length != words[windex].Length) ;
+        }
         guesses[attempt] = wordGuessed;
         //fixes with the letters colours so that it changes acordingly
-        for (int i = 0; i < lettersGuessed.Count; i++)
+        if (Medical == true) { 
+            for (int i = 0; i < lettersGuessed.Count; i++)
+            {
+                Console.SetCursorPosition(left + i, top + attempt);
+                if (lettersGuessed[i] == words[Mwindex][i])
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                }
+                else if (words[Mwindex].Contains(lettersGuessed[i]))
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                }
+                Console.WriteLine(lettersGuessed[i]);
+            }
+         }
+        else
         {
-            Console.SetCursorPosition(left + i, top + attempt);
-            if (lettersGuessed[i] == words[windex][i])
+            for (int i = 0; i < lettersGuessed.Count; i++)
             {
-                Console.ForegroundColor = ConsoleColor.Green;
+                Console.SetCursorPosition(left + i, top + attempt);
+                if (lettersGuessed[i] == words[windex][i])
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                }
+                else if (words[windex].Contains(lettersGuessed[i]))
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                }
+                Console.WriteLine(lettersGuessed[i]);
             }
-            else if (words[windex].Contains(lettersGuessed[i]))
-            {
-                Console.ForegroundColor = ConsoleColor.Yellow;
-            }
-            else
-            {
-                Console.ForegroundColor = ConsoleColor.DarkRed;
-            }
-            Console.WriteLine(lettersGuessed[i]);
         }
         //clears everything so that the next line wont have a colour or keep lettersGueesed filled
         Console.ResetColor();
         lettersGuessed.Clear();
-
+        if (Medical == true)
+        {
+            if (wordGuessed == words[windex])
+            {
+                won = true;
+                //stops it from countinuing to loop after youve guessed correctly
+                break;
+            }
+        }
         //checks if youve guessed the correct word
-        if (wordGuessed == words[windex])
+        else
         {
-            won = true;
+            if (wordGuessed == words[windex])
+            {
+                won = true;
+                //stops it from countinuing to loop after youve guessed correctly
+                break;
+            }
         }
-        //stops it from countinuing to loop after youve guessed correctly
-        if (won == true)
-        {
-            break;
-        }
+
     }
 
     Console.ResetColor(); //removes colour
     Console.SetCursorPosition(0, 26); //fixes coursor position for where we want end text to go
-    //lets you know if you have won or lost
-   
-    if (won == true)
+                                      //lets you know if you have won or lost
+    if (Medical == true)
     {
-        Console.WriteLine($"congrats! you guessed corectly, the word was {words[windex]}");
-        gamesWon = gamesWon + 1;
-    }
-    else
-    {
-        Console.WriteLine($"Game Over! The correct word was;{words[windex]}");
+        if (won == true)
+        {
+            Console.WriteLine($"congrats! you guessed corectly, the word was {words[Mwindex]}");
+            gamesWon = gamesWon + 1;
+        }
+        else
+        {
+            Console.WriteLine($"Game Over! The correct word was;{words[Mwindex]}");
 
+        }
+    }
+    else 
+    {
+        if (won == true)
+        {
+            Console.WriteLine($"congrats! you guessed corectly, the word was {words[windex]}");
+            gamesWon = gamesWon + 1;
+        }
+        else
+        {
+            Console.WriteLine($"Game Over! The correct word was;{words[windex]}");
+
+        }
     }
     Console.WriteLine("");
     Console.WriteLine("would you like to restart?");
     Console.WriteLine("Yes[1]          No[2]");
     Console.WriteLine("");
-    restart = char.ToLower(Console.ReadKey().KeyChar);
-    //it lets you restart the game but keeps score or let you exit it properly
-    if (restart == '1')
-    {
-        won = false;
-        game = true;
-        Console.Clear();
-        lettersGuessed = new List<char>();
-    }
-    else if (restart == '2')
-    {
-        game = false;
-    }
+
+        restart = char.ToLower(Console.ReadKey().KeyChar);
+        //it lets you restart the game but keeps score or let you exit it properly
+        if (restart == '1')
+        {
+            won = false;
+            game = true;
+            Console.Clear();
+            lettersGuessed = new List<char>();
+        }
+        else if (restart == '2')
+        {
+            game = false;
+        }
     Console.WriteLine("");
 }
 Console.WriteLine("ending game now");
